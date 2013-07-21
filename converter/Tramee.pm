bbdbbd88035b23a59ca5ff6f2b77e2e7074f2a37
eXerigumo Clanjor (哆啦比猫) <cjxgm@126.com>
2013-07-21 22:47:00 +0800

    @ many buf fixes, tramee2svg nearly done

--- converter/Tramee.pm
+++ converter/Tramee.pm
@@ -15,10 +15,11 @@ sub load
 	my @fields = split/\s+/;
 
 	sub slash_to_empty { "@_" eq "/" ? "" : "@_" }
+	return () if $fields[1] eq '/';
 
 	{
 		gender => $fields[0],
-		name   => &slash_to_empty($fields[1]),
+		name   => $fields[1],
 		note   => [ split("；", &slash_to_empty($fields[2])) ],
 		childs => [ map &load($f), (1 .. $fields[3]) ],
 	}
